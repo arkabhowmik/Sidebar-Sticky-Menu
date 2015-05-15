8644c8884ae25329db8b33a6e83f81ca4a918ab2
Arka <bhowmikarka@gmail.com>
2015-05-15 17:53:47 +0530

    Double Click Working. Very Crude technique.

--- scripts.js
+++ scripts.js
@@ -1,29 +1,37 @@
 jQuery(document).ready(function ($) {
-    var menu = $('#cssmenu');
+    var menu = $('#cssmenu'),clicks = 0, timer = null;
 
     if(Modernizr.touch){
 
-        menu.find('li.has-sub > a').click(function(e){
-
-            var href = $(this).attr('href');
-
-            if (!$(this).data('timer')) {
-                $(this).data('timer', setTimeout(function () {
-                    console.log('Waiting');
-                }, 1000));
+        menu.find('li.has-sub > a').on('click',function(e){
+            e.preventDefault();
+            clicks++;
+            if(clicks == 1){
+                timer = setTimeout(function() {
+                    clicks = 0;
+                }, 700);
+            } else {
+                clearTimeout(timer);
+                window.location = $(this).attr('href');
             }
-
             return false;
 
         });
 
-        menu.find('a').dblclick(function(){
-            clearTimeout($(this).data('timer'));
-            $(this).data('timer', null);
-            console.log('Double Click');
-            window.location = $(this).attr('href');
-
-            return false;
+        menu.find('a').on('dblclick',function(e){
+            e.preventDefault();
         });
     }
+
+    var $buffer = false;
+    $(".menu-toggler").on('click',function () {
+        console.log('Yeah');
+        if(!$buffer){
+            $('#cssmenu').removeClass('menu-toggle');
+            $buffer = true;
+        } else{
+            $('#cssmenu').addClass('menu-toggle');
+            $buffer = false;
+        }
+    });
 });
