7510d663ea5a2c850796e023642a32f0a9b7aed8
Arka <bhowmikarka@gmail.com>
2015-05-15 10:33:43 +0530

    Removed Click events from parent items having dropdowns. But double click not working.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,29 @@
 jQuery(document).ready(function ($) {
     var menu = $('#cssmenu');
-    menu.find('a').click(function(e){
-        e.preventDefault();
-    });
-    menu.find('a').dblclick(function(){
-        window.location = $(this).attr('href');
-    });
+
+    if(Modernizr.touch){
+
+        menu.find('li.has-sub > a').click(function(e){
+
+            var href = $(this).attr('href');
+
+            if (!$(this).data('timer')) {
+                $(this).data('timer', setTimeout(function () {
+                    console.log('Waiting');
+                }, 1000));
+            }
+
+            return false;
+
+        });
+
+        menu.find('a').dblclick(function(){
+            clearTimeout($(this).data('timer'));
+            $(this).data('timer', null);
+            console.log('Double Click');
+            window.location = $(this).attr('href');
+
+            return false;
+        });
+    }
 });
